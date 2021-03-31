81a2d379838381bd093207e0e73d0af7218bc850
Sufyan <khan.sufyan.cs@gmail.com>
2021-03-31 18:30:07 -0400

    Adds constants

--- src/constants.ts
+++ src/constants.ts
@@ -3,3 +3,4 @@ export const IS_PROD = process.env.NODE_ENV === "production";
 export const PORT = 4000;
 export const COOKIE_MAX_AGE = 1000 * 60 * 60 * 24 * 365 * 7; // 7 years
 export const COOKIE_SECRET = "apple";
+export const FRONT_END_URL = "http://localhost:3000";
