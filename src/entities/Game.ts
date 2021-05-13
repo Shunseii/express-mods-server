0bc98631730db2f1e255f523750d0bb95d660a66
Sufyan <khan.sufyan.cs@gmail.com>
2021-05-13 15:51:24 -0400

    Adds Game entity

---
+++ src/entities/Game.ts
@@ -0,0 +1,46 @@
+import { Field, Int, ObjectType } from "type-graphql";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  BaseEntity,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToMany,
+} from "typeorm";
+import { Mod } from "./Mod";
+
+@ObjectType()
+@Entity("games")
+export class Game extends BaseEntity {
+  @Field(() => Int)
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Field()
+  @CreateDateColumn({ type: "timestamptz" })
+  createdAt!: Date;
+
+  @Field()
+  @UpdateDateColumn({ type: "timestamptz" })
+  updatedAt!: Date;
+
+  @Field()
+  @Column({ unique: true })
+  name!: string;
+
+  @Field()
+  @Column({ unique: true })
+  gameSlug!: string;
+
+  @Field()
+  @Column({ unique: true })
+  imageName!: string;
+
+  @Field()
+  @Column()
+  description!: string;
+
+  @OneToMany(() => Mod, (mod) => mod.game)
+  mods?: Mod[];
+}
