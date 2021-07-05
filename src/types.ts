d9801ac8fcf6a6163198bfdef92a2c1ac0d3bd23
Sufyan <khan.sufyan.cs@gmail.com>
2021-07-05 12:10:24 -0400

    Adds data loader instances to context type def

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,9 @@
 import { Request, Response } from "express";
 import { Redis } from "ioredis";
+import createLikeLoader from "./utils/createLikeLoader";
+import createLikesCountLoader from "./utils/createLikesCountLoader";
+
+import createUserLoader from "./utils/createUserLoader";
 
 declare module "express-session" {
   interface SessionData {
@@ -11,4 +15,7 @@ export interface Context {
   req: Request;
   res: Response;
   redis: Redis;
+  userLoader: ReturnType<typeof createUserLoader>;
+  likeLoader: ReturnType<typeof createLikeLoader>;
+  likesCountLoader: ReturnType<typeof createLikesCountLoader>;
 }
